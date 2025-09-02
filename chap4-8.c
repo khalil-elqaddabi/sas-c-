da2fd26de77a64ec797e620c6e4f142892b5b1e3
khalilelqaddabi9@gmail.com <khalilelqaddabi9@gmail.com>
2025-09-02 18:03:59 +0200

    v3

--- chap4-8.c
+++ chap4-8.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(){
-float pret,interet,pai,intmen,sold1,sold2,sold3;
+float pret,interet,pai,in,sold1,sold2,sold3;
 printf("Entrez le montant du pret : ");
 scanf("%f",&pret);
 printf("Entrez le taux d'interet: ");
 scanf("%f",&interet);
 printf("Entrez le paiement mensuel :");
 scanf("%f",&pai);
-intmen=interet/100/12;
-sold1=(pret-pai)+pret*intmen;
-sold2=(sold1-pai)+sold1*intmen;
-sold3=(sold2-pai)+sold2*intmen;
+in=interet/100/12;
+sold1=(pret-pai)+pret*in;
+sold2=(sold1-pai)+sold1*in;
+sold3=(sold2-pai)+sold2*in;
 printf("Solde restant apres le premier paiement : %.2f\n",sold1);
 printf("Solde restant apres le deuxieme paiement : %.2f\n",sold2);
 printf("Solde restant apres le troisieme paiement : %.2f\n",sold3);

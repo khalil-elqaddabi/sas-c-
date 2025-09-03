0c9f4a548806227603c890159d55a79b6ea902df
2004GG <elqaddabikhalil@gmail.com>
2025-09-03 20:06:58 +0100

    add-ex9&10

---
+++ day2/chap3-ex10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main()
+{
+      printf("%ld\n%d\n%ld\n%ld\n%ld\n%lld",sizeof(int),sizeof(short),sizeof(long),sizeof(float),sizeof(double),sizeof(long double));
+   
+    return 0;
+}
